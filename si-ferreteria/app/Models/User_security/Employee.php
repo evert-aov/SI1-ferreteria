b2a7eb20a82d87e3a33587788d29684e8b08277d
SCANDAL9 <duglasrivero2@gmail.com>
2025-12-14 01:04:38 -0400

    feat: Enhance Employee model with start and end time attributes

--- si-ferreteria/app/Models/User_security/Employee.php
+++ si-ferreteria/app/Models/User_security/Employee.php
@@ -2,21 +2,29 @@
 
 namespace App\Models\User_security;
 
-use App\Models\User;
+
+use App\Models\User_security\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
+    protected $primaryKey = 'user_id';
+    public $incrementing = false;
+
     protected $fillable = [
         'user_id',
         'salary',
+        'start_time',
+        'end_time',
         'hire_date',
         'termination_date',
     ];
 
     protected $casts = [
         'salary' => 'decimal:2',
+        'start_time' => 'datetime:H:i',
+        'end_time' => 'datetime:H:i',
         'hire_date' => 'date',
         'termination_date' => 'date',
     ];
